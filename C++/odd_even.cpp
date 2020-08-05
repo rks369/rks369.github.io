1941925889a301863e65b94d8048d44bbd63716c
Ritesh <rks369@users.noreply.github.com>
2020-08-05 21:25:29 +0530

    Odd Evevn

---
+++ C++/odd_even.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+int n;
+
+    cout<<"Enter the value of N:--";
+    cin>>n;
+    if (n%2==0)
+        cout<<"No. is Even \n";
+    else
+        cout<<"No. is Odd \n";
+    return 0;
+}
+
+
