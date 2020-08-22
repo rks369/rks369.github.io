5511f3759b2be636ee71f52c412559ba7c5c0fc9
root <riteshsaini331@gmail.com>
2020-08-22 21:42:27 +0530

    main file Added

---
+++ C++/001main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"\n";
+    
+    //Code Here
+    
+
+    cout<<"\n\n";
+    return 0;
+}
---
+++ C/000main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("\n");
+
+    //Code Here
+
+
+
+    printf("\n\n");
+    return 0;
+}
